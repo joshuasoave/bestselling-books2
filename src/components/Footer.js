1aced50a0aeddb89b0428ef6db26446b3b93ec9a
Joshua <joshua.soave@gmail.com>
2023-07-30 18:41:16 -0400

    style redisign

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,23 @@ const Footer = () => {
   return (
     <footer>
       <div className="footerContent">
-        <div>
+        <div className="footerCredits">
           <h3>By: Joshua Soave</h3>
           <h5>Book icon by Icons8</h5>
         </div>
-
-        <a
-          href="https://developer.nytimes.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            alt="new york times logo"
-            src="https://developer.nytimes.com/files/poweredby_nytimes_200a.png?v=1583354208344"
-          ></img>
-        </a>
+        <div class="footerLogo">
+          <a
+            href="https://developer.nytimes.com"
+            target="_blank"
+            rel="noreferrer"
+            class="footerLink"
+          >
+            <img
+              alt="new york times logo"
+              src="https://developer.nytimes.com/files/poweredby_nytimes_200a.png?v=1583354208344"
+            ></img>
+          </a>
+        </div>
       </div>
     </footer>
   );
